dd49fc7e02837896b4389d6ef424e8880c361242
Max Quiet <qbite0@gmail.com>
2019-11-20 18:13:06 +0700

    Basic 3D FPS Controller

---
+++ Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour
+{
+    [Header("Main")]
+    public float Sensitivity = 3f;
+
+    public Vector2 Rotation = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Rotation += new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * Sensitivity;
+
+        Rotation.x = ClampAngle(Rotation.x, -90f, 90f);
+        Rotation.y = ClampAngle(Rotation.y, -360f, 360f);
+
+        transform.rotation = Quaternion.Euler(-Rotation.x, Rotation.y, 0);
+    }
+
+    public float ClampAngle(float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F))
+        {
+            if (angle < -360F) angle += 360F;
+            if (angle > 360F) angle -= 360F;
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    [Header("Camera")]
+    public FPSCamera Camera;
+
+    [Header("Main")]
+    public float Speed = 6f;
+    public float Jump = 6f;
+
+    [Header("Ground")]
+    public bool IsGrounded;
+    public LayerMask GroundMask;
+
+    private CharacterController Controller;
+    private Vector3 Movement = Vector3.zero;
+
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        IsGrounded = Physics.CheckSphere(transform.position, Controller.radius + Controller.skinWidth + 0.01f, GroundMask);
+
+        Movement = new Vector3(Input.GetAxis("Horizontal") * Speed, Movement.y, Input.GetAxis("Vertical") * Speed);
+
+        if (IsGrounded)
+        {
+            Movement.y = 0;
+
+            if (Input.GetButton("Jump"))
+                Movement.y = Jump;
+        }
+
+        if (Controller.collisionFlags == CollisionFlags.Above)
+            Movement.y = -2f;
+
+        Movement.y += Physics.gravity.y * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0, Camera.Rotation.y, 0);
+
+        Controller.Move(transform.TransformDirection(Movement) * Time.deltaTime);
+    }
+}
